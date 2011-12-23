84083fb06dd0c95061ec9b1e0fd874d8b1a74bda
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2011-12-23 15:29:34 -0500

    Fixed a bug for the file names.

--- PairedDeNovoAssembly.sh
+++ PairedDeNovoAssembly.sh
@@ -11,7 +11,7 @@ for r1 in $(ls $sampleDirectory|grep R1)
 do
 	r2=$(echo $r1|sed 's/R1/R2/g')
 
-	pairs=$pairs" -p $r1 $r2 "
+	pairs=$pairs" -p $sampleDirectory/$r1 $sampleDirectory/$r2 "
 done
 
 command="mpiexec -n $processors Ray -k $kmerLength $pairs -o $outputDirectory"
