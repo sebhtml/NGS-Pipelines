d9d586991e4efc0a44d8a4d06dfda9a73e4d8c32
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2011-12-23 21:39:01 -0500

    The aligned counter is now generic

---
+++ CountAligned.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+token=$1
+
+for i in $(find .|grep sorted.bam)
+do  
+	total=$(/rap/nne-790-ab/software/samtools-0.1.18/samtools view $i|wc -l)
+	aligned=$( /rap/nne-790-ab/software/samtools-0.1.18/samtools view $i|grep $token|wc -l)
+	ratio=$(echo "scale=4;100*$aligned/$total"|bc)
+	name=$(basename $i)
+
+	echo "$name $total $aligned $ratio%"
+done
