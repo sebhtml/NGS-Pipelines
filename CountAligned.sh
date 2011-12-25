921eead550188ef6c3779dbbc1beeb3bc2778908
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2011-12-25 12:19:19 -0500

    Merge duplicates

--- CountAligned.sh
+++ CountAligned.sh
@@ -4,8 +4,8 @@ token=$1
 
 for i in $(find .|grep sorted.bam)
 do  
-	total=$(/rap/nne-790-ab/software/samtools-0.1.18/samtools view $i|wc -l)
-	aligned=$( /rap/nne-790-ab/software/samtools-0.1.18/samtools view $i|grep $token|wc -l)
+	total=$(/rap/nne-790-ab/software/samtools-0.1.18/samtools view $i|awk '{print $1}'|sort|uniq|wc -l)
+	aligned=$( /rap/nne-790-ab/software/samtools-0.1.18/samtools view $i|grep $token|awk '{print $1}'|sort|uniq|wc -l)
 	ratio=$(echo "scale=4;100*$aligned/$total"|bc)
 	name=$i
 
