f67f661188412253931b1c6de43ef8eee7cbf4b4
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-01-09 21:26:29 -0500

    Adding delayed launcher.

---
+++ DelayedLauncher.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+group=$1
+# assuming 32 cores per job, this is 512 cores
+maximumJobs=16
+
+userName=$(whoami)
+
+for i in $(cat SampleList.txt)
+do
+	# wait until some jobs finish
+	while test $(qstat|grep $userName|wc -l) -gt $(($maximumJobs-1))
+	do	
+		sleep 1
+	done
+
+	qsub $group$i.sh
+done
+	
