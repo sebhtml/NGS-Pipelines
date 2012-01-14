507f3c9e1a978febc4656184987a4d744efa8784
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-01-14 10:51:38 -0500

    Added other things to be detected.

--- DetectLife.sh
+++ DetectLife.sh
@@ -2,21 +2,26 @@
 
 detectLife(){
 
-group=$1
-
-thresholds="0.0001 0.001 0.01 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9"
-selection=0.5
-
-cat $group-Genomes/* > $group-Genomes.tsv
-
-for i in $thresholds
-do
-	detected=$(filter.py $group-Genomes.tsv $i|wc -l)
-	echo "$i $detected"
-done > $group-Genomes-thresholds.tsv
-
-filter.py $group-Genomes.tsv $selection > $group-Genomes-$selection.tsv
+	group=$1
+	
+	thresholds="0.0001 0.001 0.01 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9"
+	selection=0.5
+	
+	cat $group/* > $group.tsv
+	
+	for i in $thresholds
+	do
+		detected=$(filter.py $group.tsv $i|wc -l)
+		echo "$i $detected"
+	done > $group-thresholds.tsv
+	
+	filter.py $group.tsv $selection > $group-$selection.tsv
 }
 
-detectLife Bacteria
-detectLife Viruses
+detectLife Bacteria-Genomes
+detectLife Bacteria-ProteinCodingGenes
+detectLife Bacteria-RNAGenes
+detectLife Viruses-Genomes
+detectLife Viruses-ProteinCodingGenes
+detectLife Viruses-RNAGenes
+
