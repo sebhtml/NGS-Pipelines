d0ada38ad44706f88469244542590f20c7a39516
Sébastien Boisvert <seb@boisvert.info>
2012-01-26 14:34:32 -0500

    I updated the path where search directories are.

--- LoadModules.sh
+++ LoadModules.sh
@@ -3,7 +3,7 @@
 # this is the only file containing paths that needs to be changed to run on another 
 # computer
 
-export DARK_FISH_TECHNOLOGY_BASE_SEARCH_DIR=/rap/nne-790-ab/genomes/RayKmerSearchStuff/2011-12-23
+export DARK_FISH_TECHNOLOGY_BASE_SEARCH_DIR=/rap/nne-790-ab/genomes/RayKmerSearchStuff/2012-01-26
 
 DARK_FISH_TECHNOLOGY_DIRECTORY=/rap/nne-790-ab/software/NGS-Pipelines/
 BWA_PATH=/rap/nne-790-ab/software/bwa-0.6.1
