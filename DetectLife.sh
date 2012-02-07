4d2edf9f5dfe0a3c570ffc77bf5b30a65e448579
Sébastien Boisvert <seb@boisvert.info>
2012-02-07 10:32:28 -0500

    The threshold is now done in Ray.

--- DetectLife.sh
+++ DetectLife.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
 detectLife(){
-
 	group=$1
 	echo "[DetectLife] detecting things in $group"
-	
-	thresholds="0.0001 0.001 0.01 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9"
-	selection=0.5
 	
 	cat $(ls $group/*|grep -v Identifi) |grep '^#'|head -n1 > $group.tsv
 
 	cat $(ls $group/*|grep -v Identifi) |grep -v '^#' >> $group.tsv
-	
-	for i in $thresholds
-	do
-		detected=$(filter.py $group.tsv $i|wc -l)
-		echo "$i $detected"
-	done > $group-thresholds.tsv
-	
-	filter.py $group.tsv $selection > $group-$selection.tsv
 }
 
 for i in $(ls|grep -v tsv|grep -v DeNovoAssembly)
