00e3e9e64ffb552a68669715ee2bb4b8e08b0f21
Sébastien Boisvert <seb@boisvert.info>
2012-03-08 11:52:12 -0500

    A new script that can convert a whole project to html was added.

---
+++ Convert-XML-to-HTML-GreenRay-output.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# automatically create html reports from XML data files
+# author Sébastien Boisvert
+# needs xsltproc
+
+project=$1
+
+echo "Project: $project"
+
+for i in $(ls $project)
+do 
+	echo "Sample: $i"
+	for j in $(find $project/$i|grep SequenceAbundances.xml$)
+	do
+		echo "File: $j"
+		sed -i 's/&/ /g' $j
+		xsltproc /home/sboisver12/git-clones/ray/scripts/xsl-xml/SequenceAbundances-to-html.xsl $j > $j-entries.html
+		xsltproc /home/sboisver12/git-clones/ray/scripts/xsl-xml/SequenceAbundances-to-html-tables.xsl $j > $j-tables.html
+	done
+
+	for j in $(find $project/$i|grep Taxons.xml$)
+	do
+		echo "File: $j"
+		sed -i 's/&/ /g' $j
+		xsltproc /home/sboisver12/git-clones/ray/scripts/xsl-xml/Taxons-to-html.xsl $j > $j-entries.html
+		xsltproc /home/sboisver12/git-clones/ray/scripts/xsl-xml/Taxons-to-html-tables.xsl $j > $j-tables.html
+	done
+done
