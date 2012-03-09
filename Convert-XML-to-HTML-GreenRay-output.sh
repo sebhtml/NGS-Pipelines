1787492dec5fe6be927491657a330e34c2854b59
Sébastien Boisvert <seb@boisvert.info>
2012-03-09 16:10:39 -0500

    SequenceAbundances.xml can be quite large and are usually not used directly.

--- Convert-XML-to-HTML-GreenRay-output.sh
+++ Convert-XML-to-HTML-GreenRay-output.sh
@@ -9,13 +9,13 @@ echo "Project: $project"
 
 for i in $(ls $project)
 do 
-	echo "Sample: $i"
-	for j in $(find $project/$i|grep SequenceAbundances.xml$)
-	do
-		echo "File: $j"
-		xsltproc /home/sboisver12/git-clones/ray/scripts/xsl-xml/SequenceAbundances-to-html.xsl $j > $j-entries.html
-		xsltproc /home/sboisver12/git-clones/ray/scripts/xsl-xml/SequenceAbundances-to-html-tables.xsl $j > $j-tables.html
-	done
+	#echo "Sample: $i"
+	#for j in $(find $project/$i|grep SequenceAbundances.xml$)
+	#do
+		#echo "File: $j"
+		#xsltproc /home/sboisver12/git-clones/ray/scripts/xsl-xml/SequenceAbundances-to-html.xsl $j > $j-entries.html
+		#xsltproc /home/sboisver12/git-clones/ray/scripts/xsl-xml/SequenceAbundances-to-html-tables.xsl $j > $j-tables.html
+	#done
 
 	for j in $(find $project/$i|grep Taxons.xml$)
 	do
