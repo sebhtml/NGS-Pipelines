2ed5dc8a75317b1c98d7549e20d74c686ca30ae9
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-07-20 15:25:10 -0400

    I improved the tool to compare tools.
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- comparison-with-MetaPhlAn/group.py
+++ comparison-with-MetaPhlAn/group.py
@@ -23,7 +23,9 @@ def addToTree(xmlFile):
 	
 		if sampleName not in data:
 			data[sampleName]={}
-	
+		else:
+			samples.append(sampleName)
+
 		for entry in node.getElementsByTagName("taxon"):
 			rank=entry.getElementsByTagName("rank")[0].childNodes[0].nodeValue
 			name=entry.getElementsByTagName("name")[0].childNodes[0].nodeValue
@@ -43,51 +45,102 @@ def addToTree(xmlFile):
 addToTree(goldStandard)
 addToTree(mine)
 
-sample='SRS065335'
 
 ranks=["kingdom","phylum","class","order","family","genus","species"]
 
-def showEntries(data,sample,rank,count):
+def showEntries(data,sample,rank,count,stream):
+
+	humanRemove={}
+	humanRemove['kingdom']='Metazoa'
+	humanRemove["phylum"]='Chordata'
+	humanRemove['class']='Mammalia'
+	humanRemove['order']='Primates'
+	humanRemove['family']='Hominidae'
+	humanRemove['genus']='Homo'
+	humanRemove['species']='Homo sapiens'
+
+	normalization={}
+	normalization['kingdom']=0
+	normalization['phylum']=0
+	normalization['class']=0
+	normalization['order']=0
+	normalization['family']=0
+	normalization['genus']=0
+	normalization['species']=0
+
+	# update values
+	for entry in data[sample][rank].items():
+		taxon=entry[0]
+
+		if taxon!=humanRemove[rank]:
+			continue
+
+		for entry2 in entry[1].items():
+			source=entry2[0]
+			value=entry2[1]
+			
+			normalization[rank]=value
+
+
 	for entry in data[sample][rank].items():
 		taxon=entry[0]
 
 		if len(entry[1].items())!=count:
 			continue
 
-		print taxon,
+		stream.write("rank="+rank+"	taxon="+taxon)
+
 		for entry2 in entry[1].items():
 			source=entry2[0]
 			value=entry2[1]
 			
-			print " "+str(value)+" ("+source+")",
+			if source=="ray.xml":
+				value=(value/(100.00-normalization[rank]))*100.00
 
-		print ""
+			stream.write("	"+str(value)+"	("+source+")")
+
+			if count==1 and value>=1:
+				stream.write("	UNIQUE")
+
+		stream.write("\n")
 
 
 def generateReport(sample):
 
-	print "sample= "+sample
-	print ""
+	stream=open(sample,"w")
+
+	stream.write("sample= "+sample)
+	stream.write("")
+
 	for rank in ranks:
 		if rank in data[sample]:
 
-			print ""
-			print "Rank= "+rank
+			stream.write("")
+			stream.write("\n")
+			stream.write("Rank= "+rank)
+			stream.write("\n")
 
-			print ""
-			print "In both"
-			print ""
-			showEntries(data,sample,rank,2)
+			stream.write("")
+			stream.write("\n")
+			stream.write("In both")
+			stream.write("\n")
+			stream.write("")
+			stream.write("\n")
+			showEntries(data,sample,rank,2,stream)
+			stream.write("\n")
 			
-			print ""
-			print "Only in 1"
-			print ""
-			showEntries(data,sample,rank,1)
+			stream.write("")
+			stream.write("\n")
+			stream.write("Only in 1")
+			stream.write("\n")
+			stream.write("")
+			stream.write("\n")
 
+			showEntries(data,sample,rank,1,stream)
 
+	stream.close()
 				
-	
-
-generateReport(sample)
+for sample in samples:
+	generateReport(sample)
 
 
