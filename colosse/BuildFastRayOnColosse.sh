ceae2f8eaaaae561d09645fa97453123ef717f84
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-08-07 18:31:27 -0400

    A new script to build Ray with popcnt was added.
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

---
+++ colosse/BuildFastRayOnColosse.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source /rap/nne-790-ab/software/NGS-Pipelines/LoadModules.sh
+
+cd ~/git-clones/ray
+
+id=$(git log|grep ^commit|head -n1|awk '{print $2}')
+builds=/rap/nne-790-ab/software/RayAppBuilds/
+
+make clean
+
+make ASSERT=n DEBUG=n HAVE_LIBZ=y HAVE_LIBBZ2=y  EXTRA=-mpopcnt J=10 PREFIX=$builds/$id
+make install
+
+cd $builds
+rm last-build
+ln -s $id last-build
+
+echo "Ray $id has been installed."
